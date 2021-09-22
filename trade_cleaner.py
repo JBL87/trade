7d8f548afe80853cc24c3e66ea0ce376e0f7282d
JBL87 <bong2242@gmail.com>
2021-09-22 22:43:44 +0900

    upload

---
+++ trade_cleaner.py
@@ -0,0 +1,384 @@
+from glob import glob
+import pandas as pd
+import time
+import conn_db
+import helper 
+
+map_df_folder = conn_db.get_path('hs코드 신성질별 분류')
+download_folder = helper.download_folder
+new_product_folder = conn_db.get_path('신성질별_수출입')
+# trade_save_path = r"C:\Users\bong2\OneDrive\DataArchive\DB_기타_출입국,수출입\00_CSV_pickle\\"
+
+@helper.timer
+def clean_trade_data_by_new_type(): # 관세청 수출입 데이터 
+    '''
+    관세청 수출입 데이터 다운받고 나서 취합하고 정리해서 hyper로 저장하는 함수
+    국가별/품목별/신성질별 dataset 전처리용 (hscode 6자리까지 있는거)
+    ''' 
+    # 공통사항: 단위
+    unit_types = ['백만', '억']
+    unit_dict = {'백만': 1000, '억': 100000}
+    
+    #신성질별 수출입 -------- -------- -------- -------- -------- --------
+    all_files = glob(conn_db.get_path('신성질별_수출입') + "*.xls")
+    df = pd.concat([pd.read_excel(file, skiprows=4) for file in all_files], axis=0).drop_duplicates()
+
+    filt1 = df['성질명'].str.startswith('(')
+    filt2 = df['성질명'].str.endswith(')')
+    filt3 = df['기간'] != '총계'
+    filt = filt1 & filt2 & filt3
+    df = df.loc[filt].reset_index(drop=True)
+
+    helper.drop_column(df, 'Unnamed')
+    # matcher = 'Unnamed'
+    # all_cols = df.columns.tolist()
+    # delete_cols = [col for col in all_cols if any(
+    #     prcnt in col for prcnt in matcher)]
+    # use_cols = list(set(all_cols)-set(delete_cols))
+
+    df['중량'] = pd.to_numeric(df['중량'].str.replace(',', ''))
+    df['금액'] = pd.to_numeric(df['금액'].str.replace(',', ''))
+    df.rename(columns={'기간': '날짜'}, inplace=True)
+
+    trade_types = df['수출입구분'].unique().tolist()  # ['수출','수입']이 결과. 아래 다른 df에서도 활용
+
+    result_df = pd.DataFrame()
+    for trade_type in trade_types:
+        temp = df.loc[df['수출입구분'] == trade_type].copy()
+        temp.rename(columns={'중량': trade_type+'중량 (ton)',
+                       '금액': trade_type+'금액 (천$)'}, inplace=True)
+        temp.drop(columns='수출입구분', inplace=True)
+        result_df = result_df.append(temp)
+    
+    result_df.reset_index(drop=True, inplace=True)
+    result_df = result_df.groupby(['날짜', '성질명']).agg(sum).reset_index()
+    result_df['성질명'] = result_df['성질명'].str[1:-1].str.strip()
+    
+    # 맵핑
+    map_df = conn_db.from_('Master_수출입품목', '신성질별_short')
+
+    df = map_df.merge(result_df, left_on='세세분류명', right_on='성질명',
+                      how='inner').drop(columns='성질명')
+
+    # 단위 변경 및 계산
+    for unit_type in unit_types:
+        for trade_type in trade_types:
+            df[f'{trade_type}금액 ({unit_type}$)'] = df[f'{trade_type}금액 (천$)'] / \
+                            unit_dict[unit_type]
+        df[f'무역규모 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] + \
+                    df[f'수입금액 ({unit_type}$)']
+        df[f'무역수지 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] - \
+                    df[f'수입금액 ({unit_type}$)']
+    df[f'무역수지 (천$)'] = df['수출금액 (천$)'] - df['수입금액 (천$)']
+    df[f'무역규모 (천$)'] = df['수출금액 (천$)'] + df['수입금액 (천$)']
+    df['수출ton당 (천$/ton)'] = df['수출금액 (천$)'] / df['수출중량 (ton)']
+    df['수입ton당 (천$/ton)'] = df['수입금액 (천$)'] / df['수입중량 (ton)']
+    df['dataset'] = '신성질별 수출입'
+
+    [df.rename(columns={col: col[:-1]}, inplace=True)
+          for col in df.columns.tolist() if '분류명' in col]
+    df_new_type = df.copy() 
+    print('신성질별 수출입 실적 완료')
+
+    #품목별 수출입 -------- -------- -------- -------- -------- --------    
+    folder = r"C:\Users\bong2\OneDrive\DataArchive\DB_기타_출입국,수출입\3.수출입통계\1.2_품목별\\"
+    all_files = glob(folder + "*.xls")
+    df = pd.concat([pd.read_excel(file, skiprows=4)
+                 for file in all_files], axis=0).drop_duplicates()
+    filt = df['기간'] != '총계'
+    df = df.loc[filt].copy()
+    df = df.rename(columns={'기간': '날짜', '품목명': '대분류',
+                         '수출중량': '수출중량 (ton)',
+                         '수입중량': '수입중량 (ton)',
+                         '수출금액': '수출금액 (천$)',
+                         '수입금액': '수입금액 (천$)',
+                         '무역수지': '무역수지 (천$)'}).drop(columns='품목코드')
+
+    matcher = ['수출', '수입', '무역']
+    all_cols = df.columns.tolist()
+    value_cols = [col for col in all_cols if any(
+        prcnt in col for prcnt in matcher)]
+    for col in value_cols:
+        df[col] = pd.to_numeric(df[col].str.replace(',', ''))
+    for unit_type in unit_types:
+        for trade_type in trade_types:
+            df[f'{trade_type}금액 ({unit_type}$)'] = df[f'{trade_type}금액 (천$)'] / \
+                            unit_dict[unit_type]
+        df[f'무역규모 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] + \
+                    df[f'수입금액 ({unit_type}$)']
+        df[f'무역수지 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] - \
+                    df[f'수입금액 ({unit_type}$)']
+    df[f'무역규모 (천$)'] = df['수출금액 (천$)'] + df['수입금액 (천$)']
+    df['수출ton당 (천$/ton)'] = df['수출금액 (천$)'] / df['수출중량 (ton)']
+    df['수입ton당 (천$/ton)'] = df['수입금액 (천$)'] / df['수입중량 (ton)']
+    df['dataset'] = '품목별 수출입'
+    df_old_type = df.copy()
+    
+    print('품목별 수출입 실적 완료') 
+    
+    #국가별 전체 수출입 -------- -------- -------- -------- -------- --------
+    start_time = helper.now_time()
+    folder = r"C:\Users\bong2\OneDrive\DataArchive\DB_기타_출입국,수출입\3.수출입통계\1.3_국가별전체\\"
+    all_files = glob(folder + "*.xls")
+    df = pd.concat([pd.read_excel(file, skiprows=4)
+                 for file in all_files], axis=0).drop_duplicates()
+    df = df.loc[df['기간'] != '총계'].copy()
+    df.rename(columns={'기간': '날짜', '국가명': '국가',
+                    '수출건수': '수출건수 (건)',
+                    '수입건수': '수입건수 (건)',
+                    '수출금액': '수출금액 (천$)',
+                    '수입금액': '수입금액 (천$)',
+                    '무역수지': '무역수지 (천$)'}, inplace=True)
+    matcher = ['건수', '금액', '무역']
+    all_cols = df.columns.tolist()
+    value_cols = [col for col in all_cols if any(
+        prcnt in col for prcnt in matcher)]
+    for col in value_cols:
+        df[col] = pd.to_numeric(df[col].str.replace(',', ''))
+    for unit_type in unit_types:
+        for trade_type in trade_types:
+            df[f'{trade_type}금액 ({unit_type}$)'] = df[f'{trade_type}금액 (천$)'] / \
+                            unit_dict[unit_type]
+        df[f'무역규모 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] + \
+                    df[f'수입금액 ({unit_type}$)']
+        df[f'무역수지 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] - \
+                    df[f'수입금액 ({unit_type}$)']
+    df[f'무역규모 (천$)'] = df['수출금액 (천$)'] + df['수입금액 (천$)']
+    df['수출건당 (천$/건)'] = df['수출금액 (천$)'] / df['수출건수 (건)']
+    df['수입건당 (천$/건)'] = df['수입금액 (천$)'] / df['수입건수 (건)']
+    df['dataset'] = '국가별전체'
+    
+    print('국가별 전체 수출입 실적 완료')
+    df_country = df.copy()
+    #----------------------------------------------------------------------------------------------------------------
+    #-------- -------- -------- -------- -------- 국가별 신성질별 수출입. 생략 -------- -------- -------- -------- --
+    #----------------------------------------------------------------------------------------------------------------
+    # start_time = common.now_time()
+    # map_df = common.from_db('Master_수출입품목', '신성질별_short')
+    # folder = r"C:\Users\bong2\OneDrive\DataArchive\DB_기타_출입국,수출입\3.수출입통계\1.4_국가별 신성질별\\"
+    # all_files = glob(folder + "*.xls")
+    # df_raw = pd.concat([pd.read_excel(file, skiprows=4) for file in all_files], axis=0).drop_duplicates()
+    # filt1 = df_raw['성질명'].str.startswith('(')
+    # filt2 = df_raw['성질명'].str.endswith(')')
+    # filt3 = df_raw['기간'] != '총계'
+    # filt = filt1 & filt2 & filt3
+    # df_raw = df_raw.loc[filt].copy()
+    # df_raw['중량'] = pd.to_numeric(df_raw['중량'].str.replace(',', ''))
+    # df_raw['금액'] = pd.to_numeric(df_raw['금액'].str.replace(',', ''))
+    # trade_types = df_raw['수출입구분'].unique().tolist()
+    # df = pd.DataFrame()
+    # for trade_type in trade_types:
+    #     temp = df_raw.loc[df_raw['수출입구분'] == trade_type].copy()
+    #     temp.rename(columns={'중량': trade_type+'중량 (ton)',
+    #                         '금액': trade_type+'금액 (천$)'}, inplace=True)
+    #     temp.drop(columns='수출입구분', inplace=True)
+    #     df = df.append(temp)
+    # df = df.rename(columns={'기간': '날짜'}).reset_index(drop=True)
+    # df = df.groupby(['날짜', '국가명', '성질명']).agg(sum).reset_index()
+    # df['성질명'] = df['성질명'].str[1:-1].str.strip()
+    # df = map_df.merge(df, left_on='세세분류명', right_on='성질명', how='inner').drop(columns='성질명').rename(columns={'국가명': '국가'})
+    # del df_raw, map_df
+    # for unit_type in unit_types:
+    #     for trade_type in trade_types:
+    #         df[f'{trade_type}금액 ({unit_type}$)'] = df[f'{trade_type}금액 (천$)'] / unit_dict[unit_type]
+    #     df[f'무역규모 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] + df[f'수입금액 ({unit_type}$)']
+    #     df[f'무역수지 ({unit_type}$)'] = df[f'수출금액 ({unit_type}$)'] - df[f'수입금액 ({unit_type}$)']
+    # df['수출ton당 (천$/ton)'] = df['수출금액 (천$)'] / df['수출중량 (ton)']
+    # df['수입ton당 (천$/ton)'] = df['수입금액 (천$)'] / df['수입중량 (ton)']
+    # df['dataset'] = '국가별 신성질별'
+    # dummy = [df.rename(columns={col: col[:-1]}, inplace=True) for col in df.columns.tolist() if '분류명' in col]
+    # df['날짜'] = df['날짜'].str.replace('.', '-')
+    # print('국가별 신성질별 수출입 실적 완료. 소요시간 ' + str(common.now_time() - start_time))
+    #------ ------ ------ ------ ------ ------ ------ ------
+    # 저장
+    df_all = pd.concat([df_old_type, df_new_type, df_country]).reset_index(drop=True)
+    df_all['날짜'] = pd.to_datetime(df_all['날짜'])
+    conn_db.export_(df_all,'수출입실적')
+    # print('수출입 실적 전체 취합후 저장 완료. 소요시간 ' + str(helper.now_time() - start_time_total))
+    print('수출입 실적 전체 취합후 저장 완료')
+
+    # dataset별로 구굴시트 업로드
+    df_country = df.loc[df['dataset'] == '국가별전체']  
+    df_country = df_country.dropna(axis=1, how='all').reset_index(drop=True)
+
+    cols = ['날짜', '국가', '수출금액 (천$)', '수입금액 (천$)', '무역규모 (천$)',
+            '무역수지 (천$)', '수출건수 (건)', '수입건수 (건)',
+            '수출건당 (천$/건)', '수입건당 (천$/건)']
+    df_country = df_country[cols]
+    conn_db.to_(df_country, 'DB_수출입', 'import')
+
+    df_kind = df.loc[df['dataset'] == '신성질별 수출입']
+    df_kind = df_kind.dropna(axis=1, how='all').reset_index(drop=True)
+    cols = ['날짜', '대분류', '중분류', '소분류', '세분류', '세세분류',
+            '설명', '수출중량 (ton)', '수입중량 (ton)', '수출금액 (천$)',
+            '수입금액 (천$)', '무역수지 (천$)']
+    df_kind = df_kind[cols]
+    conn_db.to_(df_kind, 'DB_수출입_신성질별', 'import')
+
+def clean_data_from_unipass_by_product():  # 별도로 다운받은 수출입실적 HS코드10자리 데이터 전처리
+    '''
+    다운받을때 마다 오류가 계속 발생해서 수작업으로 jupyter에서 진행후
+    다운로드폴더에 취합된 '취합본'만 남겨놓고 이 코드 실행
+    '''
+    print('10자리 HS코드 전처리 시작')
+    path = r"C:\Users\bong2\OneDrive\DataArchive\DB_기타_출입국,수출입\3.수출입통계\1.5_별도로 받은 파일\0_받은원본\\"
+    df = pd.read_pickle(download_folder + "취합본.pkl")
+    df = df[df['기간'] != '총계'].copy()
+    df['HS코드_6자리'] = df['품목코드'].str[:6]
+    matcher = ['수출', '수입', '무역']
+    all_cols = df.columns.tolist()
+    value_cols = [col for col in all_cols if any(
+        prcnt in col for prcnt in matcher)]
+    for col in value_cols:
+        df[col] = pd.to_numeric(df[col].str.replace(',', ''))
+    # 월별로 별도로 저장
+    for date in df['기간'].unique().tolist():
+        temp = df[df['기간'] == date].reset_index(drop=True)  # 특정월만 있는 df
+        date = date.replace('.', '년')+'월'  # 파일명에 들어갈 날짜
+        if len(temp) > 0:
+            temp.to_pickle(path + f'HSCODE_10_{date}.pkl')
+        else:
+            pass 
+    files = glob(path+'*.pkl')
+    df = pd.concat([pd.read_pickle(file) for file in files])
+    df = df.drop_duplicates().reset_index(drop=True)
+    #------------ ------------ ------------
+    df = df.loc[df['기간'] != '총계'].copy().drop(columns=['품목명'])
+    df = df.rename(columns={'기간': '날짜', '품목코드': 'HS코드_10자리',
+                         '수출중량': '수출중량 (ton)', '수입중량': '수입중량 (ton)',
+                         '수출금액': '수출금액 (천$)', '수입금액': '수입금액 (천$)',
+                         '무역수지': '무역수지 (천$)'})
+    df['날짜'] = df['날짜'].str.replace('.', '-')
+    unit_types = ['백만', '억']
+    unit_dict = {'백만': 1000, '억': 100000}
+    trade_types = ['수출', '수입']
+    for unit_type in unit_types:
+        for trade_type in trade_types:
+            df[f'{trade_type}금액 ({unit_type}$)'] = df[f'{trade_type}금액 (천$)'] / \
+                            unit_dict[unit_type]
+    df['수출ton당 (천$/ton)'] = df['수출금액 (천$)'] / df['수출중량 (ton)']
+    df['수입ton당 (천$/ton)'] = df['수입금액 (천$)'] / df['수입중량 (ton)']
+    #------------ ------------ ------------
+    # drop_cols = ['세번2단위품명','HS코드_2자리', '세번4단위품명', 'HS코드_4자리','HS코드_6자리']
+    drop_cols = ['HS코드_2자리', 'HS코드_4자리', 'HS코드_6자리']
+    code_map_df = conn_db.from_('Master_수출입품목', '신성질_HS코드품목연계').drop(columns=drop_cols)
+    df = code_map_df.merge(df, on='HS코드_10자리', how='inner')
+    # df = df[['HS코드_6자리', 'HS코드_10자리', '날짜', '수출중량 (ton)', '수입중량 (ton)', '수출금액 (천$)',
+    #         '수입금액 (천$)', '무역수지 (천$)', '수출금액 (백만$)', '수입금액 (백만$)',
+    #         '수출금액 (억$)', '수입금액 (억$)', '수출ton당 (천$/ton)', '수입ton당 (천$/ton)']]
+    path = r"C:\Users\bong2\OneDrive\DataArchive\DB_기타_출입국,수출입\3.수출입통계\1.5_별도로 받은 파일\\"
+    df.to_pickle(path+'HSCODE_10_취합본.pkl') 
+    print('10자리 HS코드 전처리 완료후 저장') 
+
+@helper.timer
+def clean_hscode_file():  # 수출입 hs코드 신성질별 분류파일 정리용
+    '''
+    수출입 hs코드 신성질별 분류파일 정리용
+    '''
+    path = conn_db.get_path('hs코드 신성질별 분류')
+    file = path + "2019년_HS_신성질별 성질별 연계(홈페이지 게재용).xlsx"
+
+    # 신성질별 map
+    df_map = pd.read_excel(file, encoding='utf-8', skiprows=1,
+                           usecols='B:O', dtype='str').drop_duplicates()
+    df_map['HS코드_6자리'] = df_map['Unnamed: 1'].str[:6]
+    cols = ['세번2단위품명', '세번4단위품명', '대분류코드', '중분류코드',
+            '소분류코드', '세분류코드', '세세분류코드']
+
+    df_map = df_map.rename(columns={'Unnamed: 1': 'HS코드_10자리'}).drop(columns=cols)
+    df_map['예시_전체'] = df_map['세번10단위품명'] + "(" + df_map['HS코드_10자리'] + ")"
+    
+    #저장
+    df_map_short = df_map.groupby(['세세분류명'], as_index=False).head(2)
+    df_map_short = df_map_short.drop(columns=['HS코드_10자리', '예시_전체'])
+    df_map_short.rename(columns={'세번10단위품명': '설명'}, inplace=True)
+    cols = ['대분류명', '중분류명', '소분류명', '세분류명', '세세분류명']
+    df_map_short = df_map_short.groupby(cols)['설명'].apply(', '.join).reset_index()
+    conn_db.to_(df_map_short, 'Master_수출입품목', '신성질별_short')
+    
+    #-------------------------------
+    cols = ['HS코드_10자리', 'HS코드_6자리', '예시_전체']
+    df_map_example = df_map.drop(columns=cols).drop_duplicates()
+    df_map_example = df_map_example.groupby(['세세분류명'])['세번10단위품명'].apply(', '.join).reset_index()
+    
+    cols = ['HS코드_10자리', '예시_전체', '세번10단위품명']
+    df_map_6code = df_map.drop(columns=cols).drop_duplicates()
+    df_map_6code = df_map_6code.groupby(['세세분류명'])['HS코드_6자리'].apply(', '.join).reset_index()
+
+    df_map_all = df_map.groupby(['세세분류명'])['예시_전체'].apply(', '.join).reset_index()
+    df_map_all = df_map_example.merge(df_map_6code, on='세세분류명').merge(df_map_all, on='세세분류명')
+    df_map_all.rename(columns={'세번10단위품명': '예시'}, inplace=True)
+    df_map_all['예시'] = [string+' 등' for string in df_map_all['예시'].str.replace(' 기타,', '')]
+
+    #합쳐서 저장--------------------------
+    df_map = df_map[['대분류명', '중분류명', '소분류명', '세분류명', '세세분류명']].drop_duplicates()
+    df_map = df_map.merge(df_map_all, on='세세분류명')
+    conn_db.to_(df_map, 'Master_수출입품목', '신성질별_long')
+    print('신성질별 분류 구글시트 업로드 완료')
+
+    # 품목별 map. 품목별로 신성징 분류랑 6자리까지 있는 HS코드랑 합치기
+    df = pd.read_excel(file, skiprows=1, usecols='B:O', dtype='str')
+    df = df.drop(columns=['대분류코드', '중분류코드', '소분류코드',
+                       '세분류코드', '세세분류코드']).drop_duplicates()
+    dummy = [df.rename(columns={col: col[:-1]}, inplace=True)
+          for col in df.columns.tolist() if '분류명' in col]
+    df['HS코드_6자리'] = df['Unnamed: 1'].str[:6]
+    df['HS코드_4자리'] = df['Unnamed: 1'].str[:4]
+    df['HS코드_2자리'] = df['Unnamed: 1'].str[:2]
+    df.rename(columns={'Unnamed: 1': 'HS코드_10자리'}, inplace=True)
+
+    #------- 관세청에서 받은 6자리 HScode명 df와 합치기------------------------
+    df_6_code = conn_db.from_('Master_수출입품목', 'HS코드품목_6자리').drop(
+        columns={'세번6단위품명(영문)', '적용개시일자'})
+    df = df.merge(df_6_code, on='HS코드_6자리', how='left').drop_duplicates()
+    df['세번6단위품명'].fillna(df['세번10단위품명'], inplace=True)
+
+    # 컬럼순서 
+    cols = ['대분류', '중분류', '소분류', '세분류', '세세분류', '세번2단위품명', 'HS코드_2자리',
+         '세번4단위품명', 'HS코드_4자리', '세번6단위품명', 'HS코드_6자리', '세번10단위품명', 'HS코드_10자리']
+    
+    # 정렬순서 
+    sort_cols = ['HS코드_2자리', '대분류', '중분류', '소분류', '세분류', '세세분류',
+              'HS코드_2자리',  'HS코드_4자리', 'HS코드_6자리', 'HS코드_10자리']
+    
+    # 컬럼 순서와 행순서 정렬 후 업로드
+    df = df[cols].sort_values(by=sort_cols, ascending=True).reset_index(drop=True)
+    conn_db.to_(df, 'Master_수출입품목', '신성질_HS코드품목연계')
+    print('품목별 HSCODE표 구글시트 업로드 완료')
+
+@helper.timer
+def istans_hs_code_table():  # istans에서 받은 코드표 정리
+    df = conn_db.from_('Master_수출입품목', 'istans_원본수정')
+    for x in ['ISTANS Code', '10차 KSIC']:
+        cols = df.columns.tolist()
+        temp = df[x].str.split(' ', expand=True)
+        df[x] = df[x].str.split(' ', expand=True)[0]
+        df = temp.merge(df, left_on=0, right_on=x, how='left')
+        df = df.melt(id_vars=cols, var_name='temp', value_name='Code').dropna(
+            subset=['Code']).reset_index(drop=True)
+        df = df.drop(columns=['temp', x]).rename(columns={'Code': x})
+
+    df_istans_ksic = df.sort_values(by=['GRC Code', 'ISTANS Code']).reset_index(drop=True)
+    conn_db.to_(df_istans_ksic, 'Master_수출입품목', 'istans_ksic')
+
+    df_istans_hs = df.drop_duplicates(subset='ISTANS Code').drop(columns='10차 KSIC')
+    df_istans_hs = df_istans_hs.sort_values(by=['GRC Code']).reset_index(drop=True)
+    df_istans_hs['len'] = df_istans_hs['ISTANS Code'].apply(len)
+
+    # 수출입품목 연게표랑 istans-hs 합치기
+    istans_hs = conn_db.from_('Master_수출입품목', 'istans_hs')
+    hs_code = conn_db.from_('Master_수출입품목', '신성질_HS코드품목연계')
+    hs_code = hs_code.merge(istans_hs, left_on='HS코드_6자리', right_on='hsc', how='inner')
+    hs_code.drop(columns=['hsc'], inplace=True)
+        
+    df = pd.DataFrame()
+    for num in [2, 3, 4]:
+        hs_code[f'isc5_{num}'] = hs_code['isc5'].str[:num]
+        hs_code_map_col = hs_code[f'isc5_{num}']
+        temp = hs_code.merge(df_istans_hs[df_istans_hs['len'] == num],
+                       left_on=hs_code_map_col, right_on='ISTANS Code')
+        df = df.append(temp)
+    for col in 'isc':
+        df = helper.drop_column(df, col)
+    df = df.drop(columns=['GRC Code', 'len']).reset_index(drop=True)
+    conn_db.to_(df, 'Master_수출입품목', 'mapping')
---
+++ trade_updater.py
@@ -0,0 +1,229 @@
+from glob import glob
+import pandas as pd
+import time
+import conn_db, helper
+
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+
+download_folder = helper.download_folder
+new_product_folder = conn_db.get_path('신성질별_수출입')
+# trade_save_path = r"C:\Users\bong2\OneDrive\DataArchive\DB_기타_출입국,수출입\00_CSV_pickle\\"
+
+# 관세청 품목별 수출입 데이터가져오기 (hs코드 10자리)
+@helper.timer
+def get_data_from_unipass_by_product_10hscode(code_list, start_date, end_date):
+    '''
+    관세청 품목별 수출입 데이터 hs코드 10자리 까지 다운받는것
+    start_date, end_date = 'yyyy.mm'
+    end_date를 최근으로 하려면 0 입력
+    '''
+    # 1. 관세청 홈페이지 들어가기
+    driver = webdriver.Chrome()
+    driver.get('https://unipass.customs.go.kr/ets/index.do')
+    time.sleep(5)
+
+    # 2. 실제 통계 목록 있는 화면으로 들어가기
+    selector = '#maincont > div > article > ul > li:nth-child(1) > div > ul.leftArea.listype01 > li:nth-child(3) > a'
+    driver.find_element_by_css_selector(selector).click()
+    time.sleep(5)
+
+    # 3. 품목별 수출입실적 메뉴 클릭
+    selector = '#left > nav > ul > li:nth-child(3) > a'
+    driver.find_element_by_css_selector(selector).click()
+    time.sleep(5)
+
+    # 품목별 실적용
+    by_items_id = '#TRS0104008Q'  # 품목별 실적 전용 ID
+
+    # 4.0 조회기간 설정
+    selector = f'{by_items_id}_priodKind > option:nth-child(2)'
+    driver.find_element_by_css_selector(selector).click()  # 조회기간 '월'로 변경
+
+    # 4.1 시작날짜 설정
+    # 시작날짜 선택 드룹다운 열기
+    driver.find_element_by_css_selector(f'{by_items_id}_priodFr').click()
+    # 시작날짜 설정
+    Select(driver.find_element_by_css_selector(f'{by_items_id}_priodFr')).select_by_visible_text(start_date)
+
+    # 4.2 종료날짜 설정
+    # 조회기간 'to' 클릭해서 dropdown 열기
+    driver.find_element_by_css_selector(f'{by_items_id}_priodTo').click()
+    if end_date == 0:  # 0일 경우 항상 가장 최근 날짜로 선택
+        Select(driver.find_element_by_css_selector(f'{by_items_id}_priodTo')).select_by_index(0)
+    else:  # parameter로 받은 종료날짜 설정
+        Select(driver.find_element_by_css_selector(f'{by_items_id}_priodTo')).select_by_visible_text(end_date)
+    # driver.find_element_by_css_selector(f'{by_items_id}_priodTo').click()  # 조회기간 'to' dropdown 닫음
+
+    # 5 품목코드 입력
+    for code in code_list:
+        # 첫 2자리 코드 입력
+        first_code = str(code[:2])
+        driver.find_element_by_id('TRS0104008Q_hsSgn02').send_keys(first_code)  # HSCODE 1,2 번째 2자리 입력
+        time.sleep(1)
+
+        # 3~4번째 자리 코드 입력
+        second_code = str(code[2:4])
+        driver.find_element_by_id('TRS0104008Q_hsSgn04').send_keys(second_code)  # HSCODE 3,4 번째 2자리 입력
+        time.sleep(1)
+
+        # if len(code)>4: # HSCODE 5,6 번째 2자리 입력. 필요한 경우 입력
+        third_code = str(code[-2:])
+        driver.find_element_by_id('TRS0104008Q_hsSgn06').send_keys(third_code)
+        time.sleep(1)
+
+        # 6 조회버튼클릭
+        selector = f'{by_items_id}_fmSearch > div > footer > button'
+        driver.find_element_by_css_selector(selector).submit()
+        time.sleep(3)
+
+        while True: # 조회되길 기다리기
+            selector = '#TRS0104008Q_table > div.blockUI.blockMsg.blockElement > div'
+            try:
+                driver.find_element_by_css_selector(selector).get_attribute("style") == ''
+                time.sleep(1)
+            except:
+                break
+        try:
+            driver.switch_to.alert.dismiss()  # 다운로드 경고 창 확인 클릭
+
+        # 7 엑셀다운로드
+        except:  
+            # 엑셀다운로드
+            driver.find_element_by_css_selector(f'{by_items_id}_downExcel_double').send_keys('\n')
+            file_count = len(glob(excel_download_folder + "*.xls"))
+            time.sleep(5)
+            try:  # 데이터가 없다는 경고창이 뜨는 경우
+                driver.switch_to.alert.dismiss()  # 다운로드 경고 창 확인 클릭
+            except:  # 다운로드 경고창이 안뜰때
+                max_wait = 5
+                # 다운로드 폴더에 있는 파일 갯수
+                while len(glob(excel_download_folder + "*.xls")) == file_count:
+                    time.sleep(3)  # 조회결가 나오기 기다리기
+                    if max_wait == 0:
+                        print('다운로드 안되는 문제 생겨서 멈춤')
+                        break
+                    else:
+                        max_wait -= 1
+
+        # 코드 입력칸 내용 지우기
+        driver.find_element_by_id('TRS0104008Q_hsSgn02').clear()
+        driver.find_element_by_id('TRS0104008Q_hsSgn04').clear()
+        if len(code) > 4:
+            driver.find_element_by_id('TRS0104008Q_hsSgn06').clear()
+        time.sleep(2)
+    driver.quit()
+
+# 신성질별 수출입 실적 다운로드
+@helper.timer
+def get_data_from_unipass_by_new_product(start_date, end_date):
+    '''
+    신성질별 수출입 실적 다운로드
+    start_date, end_date = 'yyyy.mm'
+    end_date를 최근으로 하려면 0(숫자) 입력
+    '''
+    # 1. 관세청 홈페이지 들어가기
+    driver = webdriver.Chrome()
+
+    driver.get('https://unipass.customs.go.kr/ets/index.do')
+    time.sleep(10)
+
+    # 2. 실제 통계 목록 있는 화면으로 들어가기
+    # 신성질별 수출입실적 메뉴 클릭.
+    time.sleep(10)
+    selector = '#maincont > div > article > ul > li:nth-child(1) > div > ul.leftArea.listype01 > li:nth-child(5) > a'
+    driver.find_element_by_css_selector(selector).click()
+    time.sleep(10)
+
+    # 조회기간 '월'로 변경
+    selector = '#TRS0104011Q_priodKind > option:nth-child(2)'
+    driver.find_element_by_css_selector(selector).click()
+
+    # 시작날짜 설정
+    driver.find_element_by_css_selector('#TRS0104011Q_priodFr').click()  # 시작날짜 선택 드룹다운 열기
+    Select(driver.find_element_by_css_selector('#TRS0104011Q_priodFr')).select_by_visible_text(start_date)  # 시작날짜 설정
+    # 종료날짜 설정. 조회기간 'to' 클릭해서 dropdown 열기
+    driver.find_element_by_css_selector('#TRS0104011Q_priodTo').click()
+    if end_date == 0:
+        # 항상 처음 있는 월 선택
+        Select(driver.find_element_by_css_selector('#TRS0104011Q_priodTo')).select_by_index(0)
+    else: # 종료날짜 설정
+        Select(driver.find_element_by_css_selector('TRS0104011Q_priodTo')).select_by_visible_text(end_date)
+    # driver.find_element_by_css_selector('#TRS0104011Q_priodTo').click()  # 조회기간 'to' dropdown 닫음
+
+    driver.find_element_by_css_selector('#TRS0104011Q_detailTmpr').click()  # 세부성질 클릭
+
+    # 수출선택
+    driver.find_element_by_css_selector('#TRS0104011Q_expTpcd').click()
+    # 조회클릭
+    selector = '#TRS0104011Q_fmSearch > div > footer > button'
+    driver.find_element_by_css_selector(selector).submit()
+    time.sleep(30)
+
+    try:  # 수출 엑셀다운로드
+        driver.find_element_by_css_selector('#TRS0104011Q_downExcel_double').send_keys('\n')
+    except:
+        time.sleep(10)  # 조회결과 기다리기
+        driver.find_element_by_css_selector('#TRS0104011Q_downExcel_double').send_keys('\n')
+    time.sleep(10)  # 다운로드 기다리기
+
+    # 수입선택
+    driver.find_element_by_css_selector('#TRS0104011Q_impTpcd').click()
+    # 조회버튼클릭
+    driver.find_element_by_css_selector('#TRS0104011Q_fmSearch > div > footer > button').submit()
+
+    time.sleep(3)  # 조회결과 기다리기
+
+    # 수입 엑셀다운로드
+    try: # 엑셀다운로드
+        driver.find_element_by_css_selector('#TRS0104011Q_downExcel_double').send_keys('\n')
+    except: # 조회결과 기다리기
+        time.sleep(10)
+        # 엑셀다운로드
+        driver.find_element_by_css_selector('#TRS0104011Q_downExcel_double').send_keys('\n')
+    time.sleep(10)  # 다운로드 기다리기
+    driver.quit()
+
+    #취합해서 저장하기----------------------------------
+    files = glob(download_folder + "*.xls")
+    df = pd.concat([pd.read_excel(file, encoding='euc-kr')
+                 for file in files], axis=0).drop_duplicates()
+    df.to_excel(new_product_folder +f'신성질별 수출입실적_{start_date}.xls', encoding='euc-kr', index=False)
+
+# 관세청에서 HScode 6자리 명칭 가져오기
+@helper.timer
+def get_6_hscode_from_unipass():
+    '''
+    HScode 6자리 명칭 가져와서 구글시트 업로드
+    '''
+    driver = webdriver.Chrome()
+    hs_codes_all = conn_db.from_('Master_수출입품목', '신성질_HS코드품목연계')
+    hs_codes_all = hs_codes_all['HS코드_4자리'].unique().tolist()
+
+    def get_hscode(hscode):
+        driver.get(f'https://unipass.customs.go.kr/ets/hmpg/openTRS0107001Q.do?hsSgn={hscode}&hsSgnLen=6')
+        time.sleep(3)
+
+        # 창열기
+        element = driver.find_elements_by_css_selector("#TRS0107001Q_table > tbody > tr > td")
+        element_to_list = [row.text for row in element]  # 결과 list로 추출
+        # 결과가 없는 경우가 있음. 이럴 경우 1줄로 '조회결과없다'고 나옴
+        if len(element_to_list) > 1:
+            try:
+                codes = element_to_list[1::5]  # HS부호
+                names_kr = element_to_list[2::5]  # HS부호명
+                names_en = element_to_list[3::5]  # HS부호영문명
+                apply_date = element_to_list[4::5]  # HS부호적용개시일자
+                return pd.DataFrame([codes, names_kr, names_en, apply_date]).T
+            except:
+                print(f'{hscode} 작업실패')
+        else:
+            print(f'{hscode} 결과없음')
+    df = pd.concat([get_hscode(code) for code in hs_codes_all])
+    driver.quit()
+    df = df.rename(columns={0: 'HS코드_6자리', 1: '세번6단위품명',
+                         2: '세번6단위품명(영문)', 3: '적용개시일자'}).reset_index(drop=True).drop_duplicates()
+    conn_db.to_(df, 'Master_Master_수출입품목', 'HS코드품목_6자리')
